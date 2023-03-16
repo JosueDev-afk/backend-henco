ae306d9e4de29248c4de5563cbb1b66526f8271b
Josue_Dev <josue_ceballos12@hotmail.com>
2023-03-15 20:36:36 -0600

    mensaje backend

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express()
 
+app.get('/',(req,res)=>res.send("backend"))
+
 app.listen(process.env.PORT || 3000)
 console.log('Server en el puerto',process.env.PORT || 3000)
 
