9bcf13857728778cb3d94dd4814fb2647f9da13f
Josue_Dev <josue_ceballos12@hotmail.com>
2023-03-15 22:38:40 -0600

    cuarto intento consulta a mongodb atlas

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require("./routes/user")
 const app = express()
-
-
+//middleware
+app.use('/api',userRoutes)
+app.use(express.json())
 
 app.listen(process.env.PORT || 3000)
 console.log('Server en el puerto',process.env.PORT || 3000)
 
-const { MongoClient } = require('mongodb');
+mongoose.connect("mongodb+srv://nietzshn:SCTQCvgViMZBnOy1@cluster0.fjmu5cz.mongodb.net/?retryWrites=true&w=majority").then(()=>console.log("Conectado a mongodb atlas")).catch((error)=>console.error(error))
 
-const uri = 'mongodb+srv://nietzshn:SCTQCvgViMZBnOy1@cluster0.fjmu5cz.mongodb.net/test';
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-client.connect((err) => {
-  if (err) {
-    console.error('Error al conectar a MongoDB Atlas:', err);
-  } else {
-    console.log('Conectado correctamente a MongoDB Atlas');
-    // aquí puedes trabajar con la base de datos
-  }
-});
-
-const collection = client.db('Henco').collection('Customer');
-
-collection.findOne({ customer : "CLIENTE 1" }, (err, resultado) => {
-    if (err) {
-      console.error('Error al buscar el documento:', err);
-    } else {
-      console.log('Resultado de la búsqueda:', resultado);
-    }
-  });
-  app.get('/',(req,res)=>res.send(resultado))
---
+++ src/routes/user.js
@@ -0,0 +1,4 @@
+const express = require("express")
+const router = express.Router()
+router.post("/users",(req,res)=>{res.send("create user")})
+module.exports=router
